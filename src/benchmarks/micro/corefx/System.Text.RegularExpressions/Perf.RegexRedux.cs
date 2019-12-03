189e40413dda52ca6ca5fba451a4c7f9fc183ab2
Stephen Toub <stoub@microsoft.com>
2019-12-03 15:20:20 -0500

    Change to use static methods

--- src/benchmarks/micro/corefx/System.Text.RegularExpressions/Perf.RegexRedux.cs
+++ src/benchmarks/micro/corefx/System.Text.RegularExpressions/Perf.RegexRedux.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
@@ -18,19 +19,6 @@ namespace System.Text.RegularExpressions.Tests
                 "corefx", "System.Text.RegularExpressions", "content", 
                 "200_000.in"));
 
-        static Regex regex(string re, RegexOptions options)
-        {
-             return new Regex(re, options);
-        }
-
-        static string regexCount(string s, string r, RegexOptions options)
-        {
-            int c = 0;
-            var m = regex(r, options).Match(s);
-            while (m.Success) { c++; m = m.NextMatch(); }
-            return r + " " + c;
-        }
-
         [Benchmark]
         [Arguments(RegexOptions.None)]
         [Arguments(RegexOptions.Compiled)]
@@ -42,11 +30,11 @@ namespace System.Text.RegularExpressions.Tests
 
             var magicTask = Task.Run(() =>
             {
-                var newseq = regex("tHa[Nt]", options).Replace(sequences, "<4>");
-                newseq = regex("aND|caN|Ha[DS]|WaS", options).Replace(newseq, "<3>");
-                newseq = regex("a[NSt]|BY", options).Replace(newseq, "<2>");
-                newseq = regex("<[^>]*>", options).Replace(newseq, "|");
-                newseq = regex("\\|[^|][^|]*\\|", options).Replace(newseq, "-");
+                var newseq = new Regex("tHa[Nt]", options).Replace(sequences, "<4>");
+                newseq = new Regex("aND|caN|Ha[DS]|WaS", options).Replace(newseq, "<3>");
+                newseq = new Regex("a[NSt]|BY", options).Replace(newseq, "<2>");
+                newseq = new Regex("<[^>]*>", options).Replace(newseq, "|");
+                newseq = new Regex("\\|[^|][^|]*\\|", options).Replace(newseq, "-");
                 return newseq.Length;
             });
 
@@ -61,6 +49,54 @@ namespace System.Text.RegularExpressions.Tests
             var variant8 = Task.Run(() => regexCount(sequences, "agggta[cgt]a|t[acg]taccct", options));
 
             Task.WaitAll(magicTask, variant1, variant2, variant3, variant4, variant5, variant6, variant7, variant8, variant9);
+
+            static string regexCount(string s, string r, RegexOptions options)
+            {
+                int c = 0;
+                var m = new Regex(r, options).Match(s);
+                while (m.Success) { c++; m = m.NextMatch(); }
+                return r + " " + c;
+            }
+        }
+
+        [Benchmark]
+        [Arguments(RegexOptions.None)]
+        [Arguments(RegexOptions.Compiled)]
+        public void RegexReduxMini2(RegexOptions options)
+        {
+            string sequences = input;
+            var initialLength = sequences.Length;
+            sequences = Regex.Replace(sequences, ">.*\n|\n", "", options);
+
+            var magicTask = Task.Run(() =>
+            {
+                var newseq = Regex.Replace(sequences, "tHa[Nt]", "<4>", options);
+                newseq = Regex.Replace(newseq, "aND|caN|Ha[DS]|WaS", "<3>", options);
+                newseq = Regex.Replace(newseq, "a[NSt]|BY", "<2>", options);
+                newseq = Regex.Replace(newseq, "<[^>]*>", "|", options);
+                newseq = Regex.Replace(newseq, "\\|[^|][^|]*\\|", "-", options);
+                return newseq.Length;
+            });
+
+            var variant2 = Task.Run(() => regexCount(sequences, "[cgt]gggtaaa|tttaccc[acg]", options));
+            var variant3 = Task.Run(() => regexCount(sequences, "a[act]ggtaaa|tttacc[agt]t", options));
+            var variant7 = Task.Run(() => regexCount(sequences, "agggt[cgt]aa|tt[acg]accct", options));
+            var variant6 = Task.Run(() => regexCount(sequences, "aggg[acg]aaa|ttt[cgt]ccct", options));
+            var variant4 = Task.Run(() => regexCount(sequences, "ag[act]gtaaa|tttac[agt]ct", options));
+            var variant5 = Task.Run(() => regexCount(sequences, "agg[act]taaa|ttta[agt]cct", options));
+            var variant1 = Task.Run(() => regexCount(sequences, "agggtaaa|tttaccct", options));
+            var variant9 = Task.Run(() => regexCount(sequences, "agggtaa[cgt]|[acg]ttaccct", options));
+            var variant8 = Task.Run(() => regexCount(sequences, "agggta[cgt]a|t[acg]taccct", options));
+
+            Task.WaitAll(magicTask, variant1, variant2, variant3, variant4, variant5, variant6, variant7, variant8, variant9);
+
+            static string regexCount(string s, string r, RegexOptions options)
+            {
+                int c = 0;
+                var m = Regex.Match(s, r, options);
+                while (m.Success) { c++; m = m.NextMatch(); }
+                return r + " " + c;
+            }
         }
     }
 }
